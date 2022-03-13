754234a272f97347d54a26b0db8ebd253c318895
Anvin.C.Shaju <anvincsg@gmail.com>
2022-03-13 16:57:11 +0530

    prime added

---
+++ prime.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    cout<<"Enter a number\n";
+    int num,i;
+    cin>>num;
+    for(i=2;i<=sqrt(num);i++)
+    if(num%i==0)
+    {
+        cout<<"Not Prime";
+        exit(0);
+    }
+    cout<<"Prime Number";
+    return 0;
+}
