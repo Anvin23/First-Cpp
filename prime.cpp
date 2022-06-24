0f6e3e1a16d1d6c5790db17d5e59140b90404ab8
Anvin.C.Shaju <anvincsg@gmail.com>
2022-06-24 16:14:39 +0530

    Updated prime

--- prime.cpp
+++ prime.cpp
@@ -6,6 +6,11 @@ int main()
     cout<<"Enter a number\n";
     int num,i;
     cin>>num;
+    if(num==1)
+    {
+        cout<<"Not Prime";
+        exit(0);
+    }
     for(i=2;i<=sqrt(num);i++)
     if(num%i==0)
     {
