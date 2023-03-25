0e767c2a7bfc1f02b6758a52aec30d0837fd10ce
Anvin.C.Shaju <anvincsg@gmail.com>
2023-03-25 07:19:29 +0530

    Completed Dijkstras Algo code

--- DijkstrasAlgo.cpp
+++ DijkstrasAlgo.cpp
@@ -1,38 +1,92 @@
 #include<iostream>
+#define V 6
 using namespace std;
 
-int distArray[10][10];
-bool coloredVertices[10];
+int distArray[V];
+bool coloredVertices[V];
 
-void init()
+void initialise()
 {
-    for(int i = 0;i<10;i++)
+    for(int i = 0;i<V;i++)
     {
-        for(int j=0;j<10;j++)
-        {
-            distArray[i][j] = INT32_MAX;
-        }
+        distArray[i] = INT32_MAX;
         coloredVertices[i] = false;
     }
+}
 
+bool isFull()
+{
+    for(int i=0;i<V;i++)
+    {
+        if(coloredVertices[i] == false)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int selectVertex()
+{
+    int min = INT32_MAX , min_index;
+    for(int i=0;i<V;i++)
+    {
+        if(coloredVertices[i] == false)
+        {
+            if(distArray[i] < min)
+            {
+                min_index = i;
+                min = distArray[i];
+            }
+        }
+    }
+    coloredVertices[min_index] = true;
+    return min_index;
 }
 
 int min(int a , int b)
 {
-    if(a>b)
+    if(a < b)
     {
         return a;
     }
     return b;
 }
 
-void findDistance(int *arr , int source ,int n)
+void printSolution(int source)
 {
-    distArray[source][source] = 0;
+    cout<<"\nThe shortest distance from\n";
+
+    for(int i=0;i<V;i++)
+    {
+        cout<<source<<" to "<<i<<" = "<<distArray[i]<<endl;
+    }
 }
+
+void dijkstra(int arr[V][V] , int source)
+{
+    distArray[source] = 0;
+    // coloredVertices[source] = true;
+
+    while(!isFull())
+    {
+        int u = selectVertex();
+        for(int v=0;v<V;v++)
+        {
+            if(coloredVertices[v] == false && arr[u][v] != 0)
+            {
+                distArray[v] = min(distArray[v] , distArray[u]+arr[u][v]);
+            }
+        }
+    }
+
+    printSolution(source);
+}
+
 int main()
 {
-    int graph[6][6] = {
+    int source;
+    int graph[V][V] = {
         {0,18,15,0,0,0} , 
         {18,0,6,9,0,0} , 
         {15,6,0,14,7,0} ,
@@ -40,7 +94,9 @@ int main()
         {0,0,7,10,0,36} ,
         {0,0,0,28,36,0}
     } ;
+    initialise();
 
+    dijkstra(graph , 0);
 
-
+    return 0;
 }
