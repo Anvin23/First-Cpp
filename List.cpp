09cf0c64ff0f2b1d41ede6c9ed0250edd27b2866
Anvin.C.Shaju <anvincsg@gmail.com>
2023-04-01 07:06:49 +0530

    STL List demo

---
+++ List.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+void display(list<int> &ls)
+{
+    list<int> :: iterator itr;
+    // creating an iterator
+
+    for(itr = ls.begin() ; itr != ls.end() ; itr++)
+    {
+        cout<<*itr<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    list<int> ls;
+    ls.push_back(24);
+    ls.push_back(8);
+    ls.push_back(137);
+    // adds a value at the end of the list
+    ls.push_front(91);
+    ls.push_front(40);
+    // adds a value at the beginning of the list
+
+    display(ls);
+
+    ls.pop_back();
+    // removes an element from the back
+    ls.pop_front();
+    // removes an element from the front
+
+    display(ls);
+
+    ls.remove(24);
+    // removes the given value from the list
+
+    display(ls);
+
+    return 0;
+}
