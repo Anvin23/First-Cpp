60e6cb544123dbd301c2beb8c4334cacb9651d41
Anvin.C.Shaju <anvincsg@gmail.com>
2023-04-01 06:57:05 +0530

    Updated Bank

--- Bank.cpp
+++ Bank.cpp
@@ -5,13 +5,15 @@ using namespace std;
 
 class Account
 {
-    const char* name;
+    string name;
     int id;
     float balance;
 
     public:
 
-        Account(const char* name , int id)
+        Account(){}
+
+        Account(string name , int id)
         {
            this->name = name;
            this->id = id;
@@ -28,23 +30,28 @@ class Account
             if(this->balance < amt)
             {
                 cout<<"Insufficient balance  Cannot withdraw"<<endl;
+                return;
+            }
+            else
+            {
+                this->balance -= amt;
             }
         }
 
         void displayBalance()
         {
-            cout<<"Account Name : "<<this->name<<endl;
-            cout<<"Current balance : Rs."<<this->balance<<endl;
+            cout<<"Account Name : "<< this->name<<endl;
+            cout<<"Current balance : Rs."<< this->balance<<endl;
         }
 
         void addRecord()
         {
-            
-            FILE* fp = fopen("Accounts.txt","a");
-            fprintf(fp,"Account Name : %s\n",this->name);
-            fprintf(fp,"Balance Amount : Rs.%f\n\n",this->balance);
 
-            fclose(fp);
+            ofstream out("Accounts.txt" , ios::app);
+            out<<"Account Name : "<< this->name<<endl;
+            out<<"Balance Amount : Rs."<< this->balance<<endl<<endl;
+
+            out.close();
 
         }
 
@@ -52,9 +59,43 @@ class Account
 
 int main()
 {
-    Account* acc = new Account("Anlo",122);
-    acc->deposit(70000);
-    acc->displayBalance();
+    string name;
+    int id , choice;
+    float amt;
+
+    cout<<"Enter your name : ";
+    cin>>name;
+    cout<<"Enter your id : ";
+    cin>>id;
+
+    Account *acc = new Account(name , id);
+
+    do
+    {
+        cout<<"Enter the choice\n";
+        cout<<"1.Deposit\n2.Withdraw\n3.Exit\n";
+        cin>>choice;
+        switch (choice)
+        {
+            case 1:
+                    cout<<"Enter the amount to deposit : ";
+                    cin>>amt;
+                    acc->deposit(amt);
+                    break;
+            case 2:
+                    cout<<"Enter the amount to withdraw : ";
+                    cin>>amt;
+                    acc->withdraw(amt);
+                    break;
+            case 3: break;
+            
+            default:
+                    cout<<"Invalid selection\n";
+        }
+    } while (choice != 3);
+    
     acc->addRecord();
+
+    return 0;
     
 }
