29f5020c4e8cc7f751c65dd1a89c6db5b47ec0b1
Anvin.C.Shaju <anvincsg@gmail.com>
2023-07-31 07:17:42 +0530

    Added Binary Search Tree

---
+++ BST.cpp
@@ -0,0 +1,117 @@
+#include<iostream>
+using namespace std;
+
+class BST {
+
+    class Node {
+
+        int value;
+        Node *left;
+        Node *right;
+        int height;
+
+        public:
+
+        Node(int val) {
+            value = val;
+            left = nullptr;
+            right = nullptr;
+            height = 0;
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        friend class BST;
+
+    };
+
+    Node *root = nullptr;
+
+    Node* insert(int val, Node* node) {
+        if(node == nullptr) {
+            node = new Node(val);
+            return node;
+        }
+
+        if(val < node->value) {
+            node->left = insert(val, node->left);
+        }
+
+        if(val > node->value) {
+            node->right = insert(val, node->right);
+        }
+
+        node->height = max(height(node->left), height(node->right)) + 1;
+
+        return node;
+    }
+
+    bool balanced(Node* node) {
+        if(node == nullptr) {
+            return true;
+        }
+
+        return ((abs(height(node->left) - height(node->right)) <= 1) && balanced(node->left) && balanced(node->right));
+    }
+
+    void inOrder(Node* node) {
+        if(node == nullptr) {
+            return;
+        }
+
+        inOrder(node->left);
+        cout<<node->value<<" ";
+        inOrder(node->right);
+    }
+
+    public:
+
+        BST() {
+
+        }
+
+        void inOrder() {
+            inOrder(root);
+        }
+
+        void insert(int val) {
+            root = insert(val, root);
+        }
+
+        int height(Node* node) {
+            if(node == nullptr) {
+                return -1;
+            }
+            return node->height;
+        }
+
+        bool isEmpty() {
+            return root == nullptr;
+        }
+
+        bool balanced() {
+            return balanced(root);
+        }
+};
+
+int main() {
+    BST *tree = new BST();
+
+    tree->insert(20);
+    tree->insert(14);
+    tree->insert(33);
+    tree->insert(90);
+    tree->insert(2);
+
+    tree->inOrder();
+    cout<<endl;
+
+    if(tree->balanced()) {
+        cout<<"The tree is balanced\n";
+    }
+    else {
+        cout<<"The tree is unbalanced\n";
+    }
+}
